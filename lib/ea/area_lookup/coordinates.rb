854cdd54cc81a6e0fb329fad3ae55664788f602e
Tim Crowe <tim@woodpigeon.com>
2016-03-26 15:41:12 +0000

    Replaced alias_attribute with alias
    So we can at some point remove the activesupport gem dependency

--- lib/ea/area_lookup/coordinates.rb
+++ lib/ea/area_lookup/coordinates.rb
@@ -1,11 +1,9 @@
-require "active_support/core_ext/module/aliasing"
-
 module EA
   module AreaLookup
     class Coordinates
       attr_accessor :easting, :northing
-      alias_attribute :x, :easting
-      alias_attribute :y, :northing
+      alias x easting
+      alias y northing
 
       def initialize(easting:, northing:)
         @easting = easting
